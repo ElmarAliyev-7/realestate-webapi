32cc90fed856cf4c6527f1b236c63fe24a4099ca
root <you@example.com>
2025-07-29 17:42:33 +0400

    ContactPost Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/ContactPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Domain.Entities
+{
+    public class ContactPost
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Answer { get; set; }
+        public DateTime? AnsweredAt { get; set; }
+        public int? AnsweredBy { get; set; }
+        public DateTime CreartedAt { get; set; }
+        public int? CreartedBy { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public int? LastModifiedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/ContactPostEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class ContactPostEntityConfiguration : IEntityTypeConfiguration<ContactPost>
+    {
+        public void Configure(EntityTypeBuilder<ContactPost> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Fullname).HasColumnType("nvarchar").HasMaxLength(100).IsRequired();
+            builder.Property(m => m.Email).HasColumnType("varchar").HasMaxLength(70).IsRequired();
+            builder.Property(m => m.Message).HasColumnType("nvarchar(max)").IsRequired();
+            builder.Property(m => m.Answer).HasColumnType("nvarchar(max)");
+            builder.Property(m => m.AnsweredAt).HasColumnType("datetime");
+            builder.Property(m => m.AnsweredBy).HasColumnType("int");
+            builder.Property(m => m.CreartedAt).HasColumnType("datetime").IsRequired();
+            builder.Property(m => m.CreartedBy).HasColumnType("int");
+            builder.Property(m => m.LastModifiedAt).HasColumnType("datetime");
+            builder.Property(m => m.LastModifiedBy).HasColumnType("int");
+            builder.Property(m => m.DeletedAt).HasColumnType("datetime");
+            builder.Property(m => m.DeletedBy).HasColumnType("int");
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("ContactPosts");
+        }
+    }
+}
