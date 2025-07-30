906034a9df085433fc76fd0499ee6c7a8717de1d
root <you@example.com>
2025-07-30 17:25:11 +0400

    AnnouncementMedia Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/AnnouncementMedia.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate.Domain.Commons;
+
+namespace RealEstate.Domain.Entities
+{
+    public class AnnouncementMedia : BaseEntity<int>
+    {
+        public int AnnouncementId { get; set; }
+        public string Path { get; set; }
+        public bool IsMain { get; set; }
+        public int Type { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/AnnouncementMediaEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class AnnouncementMediaEntityConfiguration : IEntityTypeConfiguration<AnnouncementMedia>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<AnnouncementMedia> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int").UseIdentityColumn(1, 1);
+            builder.Property(m => m.AnnouncementId).HasColumnType("int").IsRequired();
+            builder.Property(m => m.Path).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(m => m.IsMain).HasColumnType("bit").IsRequired();
+            builder.Property(m => m.Type).HasColumnType("int").IsRequired();
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("AnnouncementMedias");
+
+            builder.HasOne<Announcement>()
+                .WithMany()
+                .HasForeignKey(m => m.AnnouncementId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
