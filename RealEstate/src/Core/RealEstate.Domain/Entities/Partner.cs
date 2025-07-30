ea99d34aadcecc02aa6847d1778b6172767e665a
root <you@example.com>
2025-07-30 17:54:05 +0400

    Partner Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/Partner.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.Domain.Commons;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Partner : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/PartnerEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class PartnerEntityConfiguration : IEntityTypeConfiguration<Partner>
+    {
+        public void Configure(EntityTypeBuilder<Partner> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Name).HasColumnType("nvarchar").HasMaxLength(200).IsRequired();
+            builder.Property(m => m.LogoPath).HasColumnType("nvarchar").HasMaxLength(500);
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("Partners");
+        }
+    }
+}
