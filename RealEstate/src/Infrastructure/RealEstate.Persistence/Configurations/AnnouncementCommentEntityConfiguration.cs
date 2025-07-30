851ad725c49fe190faa2abada8d262c579558689
root <you@example.com>
2025-07-30 17:47:58 +0400

    AnnouncementComment Entity

---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/AnnouncementCommentEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class AnnouncementCommentEntityConfiguration : IEntityTypeConfiguration<AnnouncementComment>
+    {
+        public void Configure(EntityTypeBuilder<AnnouncementComment> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.AnnouncementId).HasColumnType("int").IsRequired();
+            builder.Property(m => m.ParentId).HasColumnType("int");
+            builder.Property(m => m.Text).HasColumnType("nvarchar(max)").IsRequired();
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("AnnouncementComments");
+        }
+    }
+}
