f22d263fcd56146c69dc42bfb6a9bfe925636171
root <you@example.com>
2025-07-30 11:56:34 +0400

    BlogPostTag Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/BlogPostTag.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.Domain.Commons;
+
+namespace RealEstate.Domain.Entities
+{
+    public class BlogPostTag : AuditableEntity
+    {
+        public int BlogPostId { get; set; }
+        public int TagId { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/BlogPostTagEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class BlogPostTagEntityConfiguration : IEntityTypeConfiguration<BlogPostTag>
+    {
+        public void Configure(EntityTypeBuilder<BlogPostTag> builder)
+        {
+            builder.Property(m => m.BlogPostId).HasColumnName("int");
+            builder.Property(m => m.TagId).HasColumnName("int");
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => new { m.BlogPostId, m.TagId });
+            builder.ToTable("BlogPostTags");
+
+            builder.HasOne<BlogPost>()
+                .WithMany()
+                .HasForeignKey(m => m.BlogPostId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne<Tag>()
+                .WithMany()
+                .HasForeignKey(m => m.TagId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
