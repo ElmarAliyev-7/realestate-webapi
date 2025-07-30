68f3714fb552c2d0d52ce59a2d497080db43e26a
root <you@example.com>
2025-07-30 17:49:52 +0400

    Category Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.Domain.Commons;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Category : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Name).HasColumnType("nvarchar").HasMaxLength(200).IsRequired();
+            builder.Property(m => m.ImagePath).HasColumnType("nvarchar").HasMaxLength(500);
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("Categories");
+        }
+    }
+}
