5b23801a4bbd4ba54085c4a2b22df6d090ca508d
root <you@example.com>
2025-07-30 17:38:09 +0400

    AnnouncementSpecification Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/AnnouncementSpecification.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Domain.Entities
+{
+    public class AnnouncementSpecification
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/AnnouncementSpecificationEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class AnnouncementSpecificationEntityConfiguration : IEntityTypeConfiguration<AnnouncementSpecification>
+    {
+        public void Configure(EntityTypeBuilder<AnnouncementSpecification> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Name).HasColumnType("nvarchar").HasMaxLength(250).IsRequired();
+            builder.Property(m => m.IsActive).HasColumnType("bit").IsRequired();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("AnnouncementSpecifications");
+        }
+    }
+}
