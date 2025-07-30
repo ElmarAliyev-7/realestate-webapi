77d55ceaca59820e56cc0422dcf1dde6f4fcc8ce
root <you@example.com>
2025-07-30 11:21:18 +0400

    BlogPost Entity

---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/BlogPostEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class BlogPostEntityConfiguration : IEntityTypeConfiguration<BlogPost>
+    {
+        public void Configure(EntityTypeBuilder<BlogPost> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Title).HasColumnType("nvarchar").HasMaxLength(500).IsRequired();
+            builder.Property(m => m.Slug).HasColumnType("varchar").HasMaxLength(500).IsRequired();
+            builder.Property(m => m.ImagePath).HasColumnType("varchar").HasMaxLength(50).IsRequired();
+            builder.Property(m => m.Body).HasColumnType("nvarchar(max)").IsRequired();
+            builder.Property(m => m.PublishedAt).HasColumnType("datetime");
+            builder.Property(m => m.PublishedBy).HasColumnType("int");
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("ContactPosts");
+        }
+    }
+}
