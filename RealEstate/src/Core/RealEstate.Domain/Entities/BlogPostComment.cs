22c7d2166c198c799d885ee45a70acbd4b106eb0
root <you@example.com>
2025-07-30 12:21:55 +0400

    BlogPostComment Entity

---
+++ RealEstate/src/Core/RealEstate.Domain/Entities/BlogPostComment.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate.Domain.Commons;
+
+namespace RealEstate.Domain.Entities
+{
+    public class BlogPostComment : BaseEntity<int>
+    {
+        public int BlogPostId { get; set; }
+        public int? ParentId { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/BlogPostCommentEntityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extentions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class BlogPostCommentEntityConfiguration : IEntityTypeConfiguration<BlogPostComment>
+    {
+        public void Configure(EntityTypeBuilder<BlogPostComment> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.BlogPostId).HasColumnType("int").IsRequired();
+            builder.Property(m => m.ParentId).HasColumnType("int");
+            builder.Property(m => m.Text).HasColumnType("nvarchar(max)").IsRequired();
+            builder.ConfigureAuditable();
+
+            builder.HasKey(m => m.Id);
+            builder.ToTable("BlogPostComments");
+
+            builder.HasOne<BlogPost>()
+                .WithMany()
+                .HasForeignKey(m => m.BlogPostId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne<BlogPostComment>()
+                .WithMany()
+                .HasForeignKey(m => m.ParentID)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
